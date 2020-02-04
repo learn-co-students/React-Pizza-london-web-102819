282984094a9dee47b5b08bcf1bc2c2bb6674a578
hiroshiuk1991 <hiroshis91@icloud.com>
2020-02-04 11:33:10 +0000

    Done.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,16 @@
 import React from "react"
 
-const Pizza = () => {
+const Pizza = (props) => {
+  const { handleChange, pizza } = props
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{props.pizza.topping}</td>
+      <td>{props.pizza.size}</td>
+      <td>{props.pizza.vegetarian ? "I am vege" : "Not vege"}</td>
+      <td><button type="button" 
+      className="btn btn-primary" 
+      onClick={() => handleChange(pizza.id)}
+      >Edit Pizza</button></td>
     </tr>
   )
 }
